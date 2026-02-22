03e748d045eb9a9cb423fa5f4ffc2cda646b2d5a
KoolShow <51787949+KoolShow@users.noreply.github.com>
2026-02-22 21:06:07 +0800

    更新为v2导入

--- minesweepervariants/impl/summon/__init__.py
+++ minesweepervariants/impl/summon/__init__.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
+from .solverv2 import ModelV2
 
-# @Time    : 2025/06/03 05:30
-# @Author  : Wu_RH
-# @FileName: __init__.py
-
-from .summon import Summon
-
-__all__ = ['Summon']
+__all__ = ['ModelV2']
