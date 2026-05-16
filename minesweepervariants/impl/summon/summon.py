f1dfe76df32340ed7d92620af8555d0971b60664
KoolShow <51787949+KoolShow@users.noreply.github.com>
2026-05-17 02:01:18 +0800

    feat(summon): 无解raise ValueError

--- minesweepervariants/impl/summon/summon.py
+++ minesweepervariants/impl/summon/summon.py
@@ -837,11 +837,12 @@ class Summon:
             drop_r=self.drop_r
         )
         if state == 0:
-            self.logger.error("题板无解 需要重新设计")
+            self.logger.error("题板无解 请检查规则约束与fill是否语义对齐")
             self.logger.error("warn board:\n" + board.show_board())
+            raise ValueError("题板无解 请检查规则约束与fill是否语义对齐")
             return None
         if state == 2:
-            self.logger.warn("题板多解 需要重新设计/+R")
+            self.logger.warn("题板多解 可能是右线规则约束过弱 需要重试/+R")
             self.logger.warn("warn board:\n" + board.show_board())
             return None
 
